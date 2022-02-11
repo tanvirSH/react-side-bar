5fc4f0a79f6ff4096681ad85773ea92e3a0a372c
tanvirSH <mtanveer.2010@gmail.com>
2022-02-12 03:45:17 +0530

    partially added menu item in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,72 @@
 import { AnimatePresence, motion } from "framer-motion";
-import { FaBars, FaHome, FaUsers } from "react-icons/fa";
+import { FaBars, FaHome, FaLock, FaMoneyBill, FaUsers } from "react-icons/fa";
 import { BiAnalyse, BiCart, BiSearch } from "react-icons/bi";
 import { MdAdminPanelSettings, MdMessage } from "react-icons/md";
 import { AiFillHeart } from "react-icons/ai";
 import { AiTwotoneFileExclamation } from "react-icons/ai";
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
+import SidebarMenu from "./SidebarMenu";
 
 const routes = [
-    {path: '/', name: 'Home', icon: <FaHome />},
-    {path: '/analytic', name: 'Analytics', icon: <BiAnalyse />},
-    {path: '/users', name: 'Users', icon: <FaUsers />},
-    {path: '/messages', name: 'Messgaes', icon: <MdMessage />},
-    {path: '/fileManager', name: 'FileManager', icon: <AiTwotoneFileExclamation />},
-    {path: '/orders', name: 'Order', icon: <BiCart />},
-    {path: '/saved', name: 'Saved', icon: <AiFillHeart />},
-    {path: '/settings', name: 'Settings', icon: <MdAdminPanelSettings />}
+    {
+        path: '/',
+        name: 'Home',
+        icon: <FaHome />
+    },
+    {
+        path: '/analytic',
+        name: 'Analytics',
+        icon: <BiAnalyse />
+    },
+    {
+        path: '/users',
+        name: 'Users',
+        icon: <FaUsers />
+    },
+    {
+        path: '/messages',
+        name: 'Messgaes',
+        icon: <MdMessage />
+    },
+    {
+        path: '/fileManager',
+        name: 'FileManager',
+        icon: <AiTwotoneFileExclamation />
+    },
+    {
+        path: '/orders',
+        name: 'Order',
+        icon: <BiCart />
+    },
+    {
+        path: '/settings',
+        name: 'Settings',
+        icon: <MdAdminPanelSettings />,
+        subRoutes: [
+            {
+                path: '/settings/profile',
+                name: 'Profile',
+                icon: <FaUsers />
+            },
+            {
+                path: '/settings/2fa',
+                name: '2FA',
+                icon: <FaLock />
+            },
+            {
+                path: '/settings/billing',
+                name: 'Billing',
+                icon: <FaMoneyBill />
+            },
+
+        ]
+
+    },
+    { path: '/saved', name: 'Saved', icon: <AiFillHeart /> }
+
 ];
-const Sidebar = ({children}) => {
+const Sidebar = ({ children }) => {
 
     const [isOpen, setIsOpen] = useState(false);
 
@@ -59,18 +108,18 @@ const Sidebar = ({children}) => {
     };
 
     return <div className="main-container">
-        <motion.div 
-            animate={{ 
-                width: isOpen ? "200px": "35px",  
+        <motion.div
+            animate={{
+                width: isOpen ? "200px" : "35px",
                 transition: {
                     duration: 0.5,
-                    type:"spring",
+                    type: "spring",
                     damping: 10
                 }
-            }} 
+            }}
             className="sidebar">
             <div className="top-section">
-                {isOpen && <motion.h1 
+                {isOpen && <motion.h1
                     initial="hide"
                     animate="show"
                     exit="hide"
@@ -84,33 +133,49 @@ const Sidebar = ({children}) => {
                     <BiSearch />
                 </div>
                 <AnimatePresence>
-                    { isOpen && <motion.input 
-                    initial="hide"
-                    animate="show"
-                    exit="hide"
-                    variants={inputAnimateion} placeholder="Search..." /> }
+                    {isOpen && <motion.input
+                        initial="hide"
+                        animate="show"
+                        exit="hide"
+                        variants={inputAnimateion} placeholder="Search..." />}
                 </AnimatePresence>
-                
+
             </div>
             <section className="routes">
-                {routes.map((route) => (
-                    <NavLink 
-                        activeclassname="active"
-                        to={route.path} 
-                        key={route.name} 
-                        className="link">
+                {routes.map((route, index) => {
+                    if (route.subRoutes) {
+                        return (
+                            <SidebarMenu 
+                            isOpen={isOpen} 
+                            linkAnimation={linkAnimation} 
+                            route={route}
+                            key={route.name}
+                            />
+                        )
+                    }
+                    return (
+                        <NavLink
+                            activeclassname="active"
+                            to={route.path}
+                            key={index}
+                            className="link"
+                        >
                             <div className="icon">{route.icon}</div>
                             <AnimatePresence>
-                                { isOpen && <motion.div
-                                initial="hide"
-                                animate="show"
-                                exit="hide"
-                                variants={linkAnimation}
-                                className="link-text">{route.name} </motion.div>
-                                }
+                                { isOpen && (
+                                    <motion.div
+                                    initial="hide"
+                                    animate="show"
+                                    exit="hide"
+                                    variants={linkAnimation}
+                                    className="link-text">{route.name} 
+                                    </motion.div>
+                                )}
                             </AnimatePresence>
-                    </NavLink>
-                ))}
+                        </NavLink>
+                    )
+                }
+                )}
             </section>
         </motion.div>
         <main>
